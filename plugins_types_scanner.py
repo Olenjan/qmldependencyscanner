65ccfc31837dca2499e6b84d6b37dbf097f9d2a0
Jan Randmets <rand.jan@gmail.com>
2018-10-23 19:26:17 +0000

    plugins.types scanner now uses data instead of filepath

--- plugins_types_scanner.py
+++ plugins_types_scanner.py
@@ -5,38 +5,35 @@ import chardet
 #Hardcoded temporarily
 filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "plugins.types")
 
-def getRegisteredImports(filepath):
-	# do magic
-	with open(filepath, 'rb') as file:
+def getRegisteredImports(data):
+    """"returns list of {\"name\": name, \"version\": version}"""
+    # do magic
 
-		import_result = []
-		
-		# read
-		data = file.read()
-		
-		# encoding
-		encoding_info = chardet.detect(data)
-		data = data.decode('%s' % encoding_info['encoding'])
-		
-		# cleanup
-		data = data.strip()
-		
-		# analyze
-		print("Analyzing plugin types")
-		re_pattern = r'exports: \[(\".*\")]' # must be stripped format
-		re_results = re.findall(re_pattern, data)
+    import_result = []
 
-		# print
-		for import_statement in re_results:
-			split_import = import_statement.replace("\"", "").split(" ")
-			#create object
-			o = {
-				"name" : split_import[0],
-				"version" : split_import[1]
-			}
-			import_result.append(o)
-		print("Exports found: %s" % len(re_results))
-		
-		return import_result
-	
-	return None
+    # encoding
+    encoding_info = chardet.detect(data)
+    data = data.decode('%s' % encoding_info['encoding'])
+
+    # cleanup
+    data = data.strip()
+
+    # analyze
+    re_pattern = r'exports: \[(\".*\")]' # must be stripped format
+    re_results = re.findall(re_pattern, data)
+
+    # print
+    for import_statement in re_results:
+        split_import = import_statement.replace("\"", "").split(" ")
+
+        #create object
+        o = {
+                "name": split_import[0],
+                "version": split_import[1]
+            }
+        import_result.append(o)
+
+    if len(import_result) == 0:
+        return None
+
+    return import_result
