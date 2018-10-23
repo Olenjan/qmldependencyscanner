6a747c65b33ba5f0cc0df499a8637bd0c81f0255
unknown <rand.jan@gmail.com>
2018-10-23 21:22:04 +0300

    scanner now functional only

--- plugins_types_scanner.py
+++ plugins_types_scanner.py
@@ -5,31 +5,38 @@ import chardet
 #Hardcoded temporarily
 filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "plugins.types")
 
-# do magic
-with open(filepath, 'rb') as file:
+def getRegisteredImports(filepath):
+	# do magic
+	with open(filepath, 'rb') as file:
 
-	# read
-	data = file.read()
-	
-	# encoding
-	encoding_info = chardet.detect(data)
-	data = data.decode('%s' % encoding_info['encoding'])
-	
-	# cleanup
-	data = data.strip()
-	
-	# analyze
-	print("Analyzing plugin types")
-	re_pattern = r'exports: \[(\".*\")]' # must be stripped format
-	re_results = re.findall(re_pattern, data)
+		import_result = []
+		
+		# read
+		data = file.read()
+		
+		# encoding
+		encoding_info = chardet.detect(data)
+		data = data.decode('%s' % encoding_info['encoding'])
+		
+		# cleanup
+		data = data.strip()
+		
+		# analyze
+		print("Analyzing plugin types")
+		re_pattern = r'exports: \[(\".*\")]' # must be stripped format
+		re_results = re.findall(re_pattern, data)
 
-	# print
-	for import_statement in re_results:
-		split_import = import_statement.replace("\"", "").split(" ")
-		#create object
-		o = {
-			"name" : split_import[0],
-			"version" : split_import[1]
-		}
-		print(o)
-	print("Exports found: %s" % len(re_results))
+		# print
+		for import_statement in re_results:
+			split_import = import_statement.replace("\"", "").split(" ")
+			#create object
+			o = {
+				"name" : split_import[0],
+				"version" : split_import[1]
+			}
+			import_result.append(o)
+		print("Exports found: %s" % len(re_results))
+		
+		return import_result
+	
+	return None
